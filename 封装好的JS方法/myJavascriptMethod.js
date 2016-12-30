a5906f68e5ab20cce94d310f3bc8deaf6159c702
aiwalle <wallelj0909@gmail.com>
2016-12-30 16:24:39 +0800

    完善了综合实例的代码

--- 封装好的JS方法/myJavascriptMethod.js
+++ 封装好的JS方法/myJavascriptMethod.js
@@ -73,3 +73,19 @@ function moveElement(elementID, final_x, final_y, interval) {
   var repeat = "moveElement('"+elementID+"', "+final_x+", "+final_y+", "+interval+")";
   elem.movement = setTimeout(repeat, interval);
 }
+
+
+function getHTTPObject() {
+  if (typeof XMLHttpRequest == "undefined") {
+    XMLHttpRequest = function() {
+      try { return new ActiveXObject("Msxml2.XMLHTTP.6.0");}
+        catch (e) {}
+      try { return new ActiveXObject("Msxml2.XMLHTTP.3.0");}
+        catch (e) {}
+      try { return new ActiveXObject("Msxml2.XMLHTTP");}
+        catch (e) {}
+      return false;
+    }
+  }
+  return new XMLHttpRequest();
+}
