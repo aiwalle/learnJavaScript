3a6fada0f09de45cd8deebfdb82c213b95173e16
aiwalle <wallelj0909@gmail.com>
2017-01-12 16:48:49 +0800

    update

--- BOM/bom.js
+++ BOM/bom.js
@@ -44,15 +44,98 @@ if (typeof pageWidth != "number") {
 // }
 // intervalId = setInterval(incrementNumber, 500);
 
-//使用超时调用示例
-var num = 0;
-var max = 10;
-function incrementNumber() {
-  num++;
-  if (num < max) {
-    setTimeout(incrementNumber, 500);
-  } else {
-    alert("Done");
+//使用超时调用示例--------这个例子和上面的作用相同，只是使用的不同的函数
+//最好使用超时调用
+// var num = 0;
+// var max = 10;
+// function incrementNumber() {
+//   num++;
+//   if (num < max) {
+//     setTimeout(incrementNumber, 500);
+//   } else {
+//     alert("Done");
+//   }
+// }
+// setTimeout(incrementNumber, 500);
+
+
+//查询字符串参数---感觉这里书上使用逗号不太对呀
+// 逗号是对的，这里一次性命名了多个变量
+// function getQueryStringArgs() {
+//   var qs = (location.search.length > 0 ? location.search.substring(1) : ""),
+//   args = {},
+//   items = qs.length ? qs.split("&") : [],
+//   item = null,
+//   name = null,
+//   value = null,
+//   i = 0,
+//   len = items.length;
+//
+//   for( i = 0; i < len; i++) {
+//     item = items[i].split("=");
+//     name = decodeURIComponent(item[0]);
+//     value = decodeURIComponent(item[1]);
+//     if (name.length) {
+//       args[name] = value;
+//     }
+//   }
+//   return args;
+// }
+//
+// //使用示例--假设查询字符串是 ?q=javascript&num=10
+// var args = getQueryStringArgs();
+// alert(args["q"]);                   // javascript
+// alert(args["num"]);                 // 10
+
+
+
+// 检测插件(在IE中无效)
+function hasPlugin(name) {
+  name = name.toLowerCase();
+  for (var i = 0; i < navigator.plugins.length; i++) {
+    if (navigator.plugins[i].name.toLowerCase().indexOf(name) > -1) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// alert(hasPlugin("Flash"));
+// alert(hasPlugin("QuickTime"));
+
+
+// 检测IE中的插件
+function hasIEPlugin(name) {
+  try {
+    new ActiveXObject(name);
+    return true;
+  } catch (e) {
+    return false;
   }
 }
-setTimeout(incrementNumber, 500);
+// alert(hasIEPlugin("ShockwaveFlash.ShockwaveFlash"));
+// alert(hasIEPlugin("QuickTime.QuickTime"));
+
+
+// 检测所有浏览器中的Flash
+function hasFlash() {
+  var result = hasPlugin("Flash");
+  if (!result) {
+    result = hasIEPlugin("ShockwaveFlash.ShockwaveFlash");
+  }
+  return result;
+}
+
+// 检测所有浏览器中的QuickTime
+function hasQuickTime() {
+  var result = hasPlugin("QuickTime");
+  if (!result) {
+    result = hasIEPlugin("QuickTime.QuickTime");
+  }
+  return result;
+}
+
+// 检测Flash
+alert(hasFlash());
+// 检测QuickTime
+alert(hasQuickTime());
