368959727ff72c3c9791043859685c33c2a8aa9f
aiwalle <wallelj0909@gmail.com>
2017-01-24 17:04:17 +0800

    update395

--- DOM2&DOM3/EventUtil.js
+++ DOM2&DOM3/EventUtil.js
@@ -83,62 +83,62 @@ var EventUtil = {
 
 
 // 确定浏览器是否支持DOM3级时间规定的HTML事件
-var isSupported = document.implementation.hasFeature("UIEvent", "3.0");
-
-// 确定浏览器是否支持DOM2级时间规定的HTML事件
-var isSupported = document.implementation.hasFeature("UIEvent", "2.0");
-
-EventUtil.addHandler(window, "load", function() {
-  var image = document.createElement("img");
-  EventUtil.addHandler(image, "load", function(event) {
-    event = EventUtil.getEvent(event);
-    // alert(EventUtil.getTarget(event).src);
-  });
-  document.body.appendChild(image);
-  image.src = "images/smile.gif";
-});
-
-// resize
-EventUtil.addHandler(window, "resize", function(event) {
-  // alert("Resized");
-});
-
-EventUtil.addHandler(window, "load", function() {
-  var script = document.createElement("script");
-  EventUtil.addHandler(script, "load", function(event) {
-    // alert("Loaded");
-  });
-  script.src = "example.js";
-  document.body.appendChild(script);
-});
-
-// load
-EventUtil.addHandler(window, "load", function() {
-  var link = document.createElement("link");
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  EventUtil.addHandler(link, "load", function(event) {
-    // alert("css loaded");
-  });
-  link.href = "example.css";
-  document.getElementsByTagName("head")[0].appendChild(link);
-});
-
-// unload
-EventUtil.addHandler(window, "unload", function() {
-  // alert("Unloaded");
-});
-
-EventUtil.addHandler(window, "scroll", function(event) {
-  if (document.compatMode == "CSS1Compat") {
-    alert(document.documentElement.scrollTop);
-  } else {
-    alert(document.body.scrollTop);
-  }
-});
-
-// 焦点事件
-var isSupported = document.implementation.hasFeature("FocusEvent", "3.0");
+// var isSupported = document.implementation.hasFeature("UIEvent", "3.0");
+//
+// // 确定浏览器是否支持DOM2级时间规定的HTML事件
+// var isSupported = document.implementation.hasFeature("UIEvent", "2.0");
+//
+// EventUtil.addHandler(window, "load", function() {
+//   var image = document.createElement("img");
+//   EventUtil.addHandler(image, "load", function(event) {
+//     event = EventUtil.getEvent(event);
+//     // alert(EventUtil.getTarget(event).src);
+//   });
+//   document.body.appendChild(image);
+//   image.src = "images/smile.gif";
+// });
+//
+// // resize
+// EventUtil.addHandler(window, "resize", function(event) {
+//   // alert("Resized");
+// });
+//
+// EventUtil.addHandler(window, "load", function() {
+//   var script = document.createElement("script");
+//   EventUtil.addHandler(script, "load", function(event) {
+//     // alert("Loaded");
+//   });
+//   script.src = "example.js";
+//   document.body.appendChild(script);
+// });
+//
+// // load
+// EventUtil.addHandler(window, "load", function() {
+//   var link = document.createElement("link");
+//   link.type = "text/css";
+//   link.rel = "stylesheet";
+//   EventUtil.addHandler(link, "load", function(event) {
+//     // alert("css loaded");
+//   });
+//   link.href = "example.css";
+//   document.getElementsByTagName("head")[0].appendChild(link);
+// });
+//
+// // unload
+// EventUtil.addHandler(window, "unload", function() {
+//   // alert("Unloaded");
+// });
+//
+// EventUtil.addHandler(window, "scroll", function(event) {
+//   if (document.compatMode == "CSS1Compat") {
+//     alert(document.documentElement.scrollTop);
+//   } else {
+//     alert(document.body.scrollTop);
+//   }
+// });
+//
+// // 焦点事件
+// var isSupported = document.implementation.hasFeature("FocusEvent", "3.0");
 
 
 // 获取鼠标事件的坐标信息
